7a18aabb9e981dbe78b2442b6ae30c146413591c
Mauricio P <mauriciopdvg@gmail.com>
2019-12-06 14:30:13 -0300

    Add Articles model

---
+++ spec/models/article_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Article, type: :model do
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_length_of(:title).is_at_least(3).is_at_most(20) }
+    it { is_expected.to validate_presence_of(:description) }
+    it { is_expected.to validate_length_of(:description).is_at_least(5).is_at_most(300) }
+  end
+end
