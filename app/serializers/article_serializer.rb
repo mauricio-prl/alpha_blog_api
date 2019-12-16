2c28dcff7338fc6572588b312d1a06f8a4b075c5
Mauricio Pereira <mauriciopdvg@gmail.com>
2019-12-16 11:15:31 -0300

    Fix the serializer of article (#8)

--- app/serializers/article_serializer.rb
+++ app/serializers/article_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ArticleSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description
+  attributes :id, :title, :description, :created_at, :updated_at
 
   belongs_to :user
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Article, type: :model do
     it { is_expected.to validate_presence_of(:title) }
     it { is_expected.to validate_length_of(:title).is_at_least(3).is_at_most(20) }
     it { is_expected.to validate_presence_of(:description) }
-    it { is_expected.to validate_length_of(:description).is_at_least(5).is_at_most(300) }
+    it { is_expected.to validate_length_of(:description).is_at_least(5).is_at_most(800) }
   end
 
   describe 'associations' do
