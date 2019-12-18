ae1b3f7ec49c0dcb71bfbc1bac8c6cf51894ebbb
Mauricio P <mauriciopdvg@gmail.com>
2019-12-18 13:59:06 -0300

    Add Admin to users

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :email
+  attributes :id, :username, :email, :admin
 
   has_many :articles
 end
