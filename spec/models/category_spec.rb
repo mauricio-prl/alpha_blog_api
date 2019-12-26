7bce70a3db9a568a02cabd619b6023b9ed73fe73
Mauricio P <mauriciopdvg@gmail.com>
2019-12-26 15:32:52 -0300

    Add Validations on database level

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,17 +3,7 @@ require 'rails_helper'
 RSpec.describe Category, type: :model do
   describe 'validation' do
     it { is_expected.to validate_presence_of(:name) }
-    it { is_expected.to validate_uniqueness_of(:name) }
-
-    describe 'same article can not be twice in the same category' do
-      let!(:category) { create(:category, name: 'Test') }
-      let!(:user) { create(:user) }
-      let!(:article) { create(:article, user: user, categories: [category]) }
-
-      it 'raises errors' do
-        expect{ article.categories << category }.to raise_error(ActiveRecord::RecordNotUnique)
-      end
-    end
+    it { create(:category); is_expected.to validate_uniqueness_of(:name) }
   end
 
   describe 'assiciations' do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:username) }
-    it { is_expected.to validate_uniqueness_of(:username).case_insensitive }
+    it { create(:user); is_expected.to validate_uniqueness_of(:username).case_insensitive }
     it { is_expected.to validate_length_of(:username).is_at_least(3).is_at_most(25) }
     it { is_expected.to validate_presence_of(:email) }
-    it { is_expected.to validate_uniqueness_of(:email) }
+    it { create(:user); is_expected.to validate_uniqueness_of(:email) }
     it { is_expected.to validate_length_of(:email).is_at_most(100) }
     it { is_expected.not_to allow_value('invalid_email@%&#exemple.com').for(:email) }
     it { is_expected.to validate_presence_of(:password) }
